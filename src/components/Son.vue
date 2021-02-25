860fa995d921a03b1cf0e7cf94777f1b91459c47
vusn <vusn@qq.com>
2021-02-25 15:06:53 +0800

    Vue常用写法回顾

---
+++ src/components/Son.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="son">
+    <p>首页子组件</p>
+    <p>{{parentdata}}</p>
+    <button @click="sonFn">子组件按钮</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Son',
+  components: {},
+  props: {
+    parentdata: {
+      type: String
+    }
+  },
+  methods: {
+    sonFn () {
+      this.$emit('parentFn')
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>首页</p>
+    <p>{{message}}</p>
+    <button @click="myFn">我是按钮</button>
+    <p>{{msg}}</p>
+    <Son :parentdata="message" @parentFn="myFn"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Son from '@/components/Son'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Son
+  },
+  // 数据绑定
+  data: function () {
+    return {
+      message: 'hzj'
+    }
+  },
+  // 方法绑定
+  methods: {
+    myFn () {
+      console.log(666)
+    }
+  },
+  // 计算属性
+  computed: {
+    msg () {
+      console.log('执行了')
+      return this.message.split('').reverse().join('')
+    }
+  },
+  // 观察属性
+  watch: {
+    message: function (newValue, oldValue) {
+      console.log(newValue, oldValue)
+    }
   }
+
 }
 </script>
