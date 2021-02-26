2027b39fdfe9bd565aea6ad1d44899acc46ecb51
vusn <vusn@qq.com>
2021-02-26 15:07:49 +0800

    完成后端权限校验

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,3 +5,5 @@ export const sendCode2Email = (data: {}) => Network.get('/emailCode', data)
 export const sendCode2Phone = (data: {}) => Network.get('/smsCode', data)
 export const loginUser = (data: {}) => Network.post('/login', data)
 export const isLogin = () => Network.get('/isLogin')
+
+export const getUsers = () => Network.get('/users')
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,18 +1,28 @@
 <template>
     <div class="">
+      <el-button @click="myFn">我是按钮</el-button>
     </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
-
+import { getUsers } from '../api/index'
 @Component({
 // 如果在类中找不到需要添加的内容,name就可以写在这个地方
   name: 'Admin',
   components: {}
 })
 export default class Admin extends Vue {
-
+  private myFn () {
+    console.log(66)
+    getUsers()
+      .then(data => {
+        console.log(data)
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
 }
 </script>
 
