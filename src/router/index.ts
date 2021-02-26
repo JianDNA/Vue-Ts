72c8ac66a345be1a375be09a5b4c100c45004710
vusn <vusn@qq.com>
2021-02-26 14:32:53 +0800

    前端使用守卫路由进行权限控制

--- src/router/index.ts
+++ src/router/index.ts
@@ -29,4 +29,19 @@ const router = new VueRouter({
   routes
 })
 
+// 全局前置守卫: 添加路由守卫, 添加权限控制
+router.beforeEach((to, from, next) => {
+  // 1.白名单, 注册或登录放行
+  if (to.path === '/login' || to.path === '/register') {
+    return next()
+  }
+  // 2.其他路由: 获取当前登录状态
+  const token = sessionStorage.getItem('token')
+  // 3. 判断是否已经登录, 如果已经登录就放行, 否则强制跳转到登录界面
+  if (!token) {
+    return next('/login')
+  }
+  next()
+})
+
 export default router
